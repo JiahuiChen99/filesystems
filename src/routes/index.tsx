fbe41e094674b9e617bcaf6f0c03df96651fd7cc
JiahuiChen99 <Jiahui1@hotmail.es>
2023-06-29 21:37:46 +0200

    feat: view transition api warning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,37 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Ext2 } from '~/components/ext2/ext2-global';
-import styles from './index.module.css'
+import { component$, $, useSignal } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Ext2 } from "~/components/ext2/ext2-global";
+import { HiXMarkSolid } from "@qwikest/icons/heroicons";
+import styles from "./index.module.css";
 
 export default component$(() => {
+  const viewAPIWarn = useSignal<boolean>(true);
+
+  const viewAPIWarnClose = $(() => {
+    viewAPIWarn.value = false;
+  });
+
   return (
     <>
       <h1>File Systems: The second Extended File System</h1>
       <div class={styles['container']}>
         <Ext2 />
       </div>
+      {viewAPIWarn.value && (
+        <div class={styles["view-api-warn"]}>
+          <HiXMarkSolid
+            onClick$={viewAPIWarnClose}
+            class={styles["view-api-warn-close"]}
+          />
+          <p>
+            This site supports View Transition, an experimental feature. For a
+            smoother experience use one of the following browsers [Chrome 111+,
+            Edge 111+, Opera 97+], and enable <i>viewTransition API</i> &
+            <i>viewTransition for navigations</i> flags at <a href="/">Flags</a>
+            .
+          </p>
+        </div>
+      )}
     </>
   );
 });
