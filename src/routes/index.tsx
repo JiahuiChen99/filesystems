30b729c3bde412a4e12445fdac21c8971b092065
Jiahui Chen <jchen@mediapro.tv>
2023-06-29 18:06:04 +0200

    feat: main router content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { Ext2 } from '~/components/ext2/ext2-global';
+import styles from './index.module.css'
 
 export default component$(() => {
   return (
     <>
+      <h1>File Systems: The second Extended File System</h1>
+      <div class={styles['container']}>
+        <Ext2 />
+      </div>
     </>
   );
 });
