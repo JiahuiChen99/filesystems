666a30f18341755aa9f4a41b9f9d0b97a0c13982
JiahuiChen99 <Jiahui1@hotmail.es>
2023-07-08 11:18:18 +0200

    feat: table with description component

---
+++ src/components/UI/table/tableWithDescription.tsx
@@ -0,0 +1,50 @@
+import { $, component$, useSignal } from "@builder.io/qwik";
+import { EXT2Table } from "~/components/UI/table/table";
+import { superblockDataProps } from "~/routes/ext2/superblock/superblockData";
+import { HiXMarkSolid } from "@qwikest/icons/heroicons";
+import MdiHorizontalExpand from "../icons/mdi-horizontal-expand";
+import styles from "./tableWithDescription.module.css";
+import MdiMemory from "../icons/mdi-memory";
+
+export const TableWithDescription = component$(
+  ({ data }: { data: superblockDataProps[] }) => {
+    const isDescriptionOpen = useSignal<boolean>(false);
+    const selectedSuperblockProp = useSignal<superblockDataProps>(data[0]);
+
+    const openPropertyDescription = $((selectedPropIndex: number) => {
+      selectedSuperblockProp.value = data[selectedPropIndex];
+      if (!isDescriptionOpen.value) isDescriptionOpen.value = true;
+    });
+
+    return (
+      <div class={styles["table-with-property-description"]}>
+        <EXT2Table data={data} clickCallback={openPropertyDescription} />
+        {isDescriptionOpen.value && (
+          <div class={styles["property-description-section"]}>
+            <HiXMarkSolid
+              class="close"
+              onClick$={() =>
+                (isDescriptionOpen.value = !isDescriptionOpen.value)
+              }
+            />
+            <h2>{selectedSuperblockProp.value.description}</h2>
+            <div class={styles["metadata"]}>
+              <div>
+                <MdiHorizontalExpand />
+                <span>Offset: </span>
+                <span>{selectedSuperblockProp.value.offset} bytes</span>
+              </div>
+              <div>
+                <MdiMemory />
+                <span>Size: </span>
+                <span>{selectedSuperblockProp.value.size} bytes</span>
+              </div>
+            </div>
+            {selectedSuperblockProp.value.info &&
+              selectedSuperblockProp.value.info()}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
