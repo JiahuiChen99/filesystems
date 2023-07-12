5462c26d37b21a4d5db4a465c4867516e2b2547e
JiahuiChen99 <Jiahui1@hotmail.es>
2023-07-12 21:32:49 +0200

    refactor: mdi memory icon default size

--- src/components/UI/icons/mdi-memory.tsx
+++ src/components/UI/icons/mdi-memory.tsx
@@ -4,8 +4,8 @@ export function MdiMemory(props: QwikIntrinsicElements["svg"], key: string) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24px"
-      height="24px"
+      width="1em"
+      height="1em"
       viewBox="0 0 24 24"
       {...props}
       key={key}
