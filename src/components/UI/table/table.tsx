028a2b6ce5ced037a969a3b05fe1a1c41879a8fc
JiahuiChen99 <Jiahui1@hotmail.es>
2023-07-16 13:44:33 +0200

    feat: dynamic table header generation

--- src/components/UI/table/table.tsx
+++ src/components/UI/table/table.tsx
@@ -2,6 +2,9 @@ import {
   type PropFunction,
   component$,
   useStylesScoped$,
+  useResource$,
+  $,
+  Resource,
 } from "@builder.io/qwik";
 import styles from "~/components/UI/table/table.css?inline";
 import { EXT2 } from "~/typings/ext2";
@@ -26,25 +29,50 @@ export const EXT2Table = component$(
       );
     };
 
+    const isStruct = $((d: any): boolean => {
+      return typeof d.offset === "number" && typeof d.size === "number";
+    });
+
+    // Probe data first object and determine Table headers
+    const tableHeaders = useResource$(async () => {
+      if (await isStruct(data[0])) {
+        return ["Offset (bytes)", "Size (bytes)", "Description"];
+      } else {
+        return ["Constant Name", "Value", "Description"];
+      }
+    });
+
+    const tableSkeleton = (
+      <div style={{ backgroundColor: "lightgrey", width: "35%" }}>Loading</div>
+    );
+
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Offset (bytes)</th>
-            <th>Size (bytes)</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d, index) => (
-            <tr key={d.id}>
-              <td>{d.offset}</td>
-              <td>{d.size}</td>
-              <td>{generatePropDescription(d, index)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Resource
+        value={tableHeaders}
+        onPending={() => tableSkeleton}
+        onResolved={(headers) => (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  {headers.map((th: string) => (
+                    <th>{th}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((d, index) => (
+                  <tr key={d.id}>
+                    <td>{d.offset}</td>
+                    <td>{d.size}</td>
+                    <td>{generatePropDescription(d, index)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
+      />
     );
   }
 );
