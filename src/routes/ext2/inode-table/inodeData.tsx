ab347bad281ed3c0ce7d3d9812db9e27a085f61a
JiahuiChen99 <Jiahui1@hotmail.es>
2023-07-17 18:38:43 +0200

    feat: inode table info

--- src/routes/ext2/inode-table/inodeData.tsx
+++ src/routes/ext2/inode-table/inodeData.tsx
@@ -164,42 +164,127 @@ export const inodeData: EXT2.Struct[] = [
     offset: 36,
     size: 4,
     description: "i_osd1",
-    info: $(() => <></>),
+    info: $(() => (
+      <>
+        <p>32bit OS dependant value.</p>
+        <h3>Hurd</h3>
+        <p>32bit value labeled as “translator”.</p>
+        <h3>Linux</h3>
+        <p>32bit value currently reserved.</p>
+        <h3>Masix</h3>
+        <p>32bit value currently reserved.</p>
+      </>
+    )),
   },
   {
-    id: "x",
+    id: "i_block",
     offset: 40,
     size: "15 * 4",
     description: "i_block",
-    info: $(() => <></>),
+    info: $(() => (
+      <>
+        <p>
+          15 x 32bit block numbers pointing to the blocks containing the data
+          for this inode. The first 12 blocks are direct blocks. The 13th entry
+          in this array is the block number of the first indirect block; which
+          is a block containing an array of block ID containing the data.
+          Therefore, the 13th block of the file will be the first block ID
+          contained in the indirect block. With a 1KiB block size, blocks 13 to
+          268 of the file data are contained in this indirect block.
+        </p>
+        <p>
+          The 14th entry in this array is the block number of the first
+          doubly-indirect block; which is a block containing an array of
+          indirect block IDs, with each of those indirect blocks containing an
+          array of blocks containing the data. In a 1KiB block size, there would
+          be 256 indirect blocks per doubly-indirect block, with 256 direct
+          blocks per indirect block for a total of 65536 blocks per
+          doubly-indirect block.
+        </p>
+        <p>
+          The 15th entry in this array is the block number of the
+          triply-indirect block; which is a block containing an array of
+          doubly-indrect block IDs, with each of those doubly-indrect block
+          containing an array of indrect block, and each of those indirect block
+          containing an array of direct block. In a 1KiB file system, this would
+          be a total of 16777216 blocks per triply-indirect block.
+        </p>
+        <p>
+          In the original implementation of Ext2, a value of 0 in this array
+          effectively terminated it with no further block defined. In sparse
+          files, it is possible to have some blocks allocated and some others
+          not yet allocated with the value 0 being used to indicate which blocks
+          are not yet allocated for this file.
+        </p>
+      </>
+    )),
   },
   {
     id: "i_generation",
     offset: 100,
     size: 4,
     description: "i_generation",
-    info: $(() => <></>),
+    info: $(() => (
+      <p>32bit value used to indicate the file version (used by NFS).</p>
+    )),
   },
   {
     id: "i_file_acl",
     offset: 104,
     size: 4,
     description: "i_file_acl",
-    info: $(() => <></>),
+    info: $(() => (
+      <>
+        <p>
+          32bit value indicating the block number containing the extended
+          attributes. In revision 0 this value is always 0.
+        </p>
+        <q>
+          Patches and implementation status of ACL under Linux can generally be
+          found at <a href="http://acl.bestbits.at/">http://acl.bestbits.at/</a>
+        </q>
+      </>
+    )),
   },
   {
     id: "i_dir_acl",
     offset: 108,
     size: 4,
     description: "i_dir_acl",
-    info: $(() => <></>),
+    info: $(() => (
+      <>
+        <p>
+          In revision 0 this 32bit value is always 0. In revision 1, for regular
+          files this 32bit value contains the high 32 bits of the 64bit file
+          size.
+        </p>
+        <q>
+          Linux sets this value to 0 if the file is not a regular file (i.e.
+          block devices, directories, etc). In theory, this value could be set
+          to point to a block containing extended attributes of the directory or
+          special file.
+        </q>
+      </>
+    )),
   },
   {
     id: "i_faddr",
     offset: 112,
     size: 4,
     description: "i_faddr",
-    info: $(() => <></>),
+    info: $(() => (
+      <>
+        <p>32bit value indicating the location of the file fragment.</p>
+        <q>
+          In Linux and GNU HURD, since fragments are unsupported this value is
+          always 0. In Ext4 this value is now marked as obsolete. In theory,
+          this should contain the block number which hosts the actual fragment.
+          The fragment number and its size would be contained in the{" "}
+          <a href="/ext2/inode-table#i_osd2">i_osd2</a>
+          structure.
+        </q>
+      </>
+    )),
   },
   {
     id: "i_osd2",
