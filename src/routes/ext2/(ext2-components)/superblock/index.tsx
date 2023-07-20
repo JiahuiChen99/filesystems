0bcfa856d56e5c6a05df9517299d305e6990515e
JiahuiChen99 <Jiahui1@hotmail.es>
2023-07-20 18:55:26 +0200

    refactor: remove ext2 superblock page styles

--- src/routes/ext2/(ext2-components)/superblock/index.tsx
+++ src/routes/ext2/(ext2-components)/superblock/index.tsx
@@ -1,6 +1,5 @@
 import { component$, useContext, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import styles from "./index.module.css";
 import {
   superblockData,
   superblockExtendedData,
