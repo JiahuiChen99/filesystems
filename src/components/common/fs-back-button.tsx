3cef3df1e8f869eaec9e6daec7537584cfec51cc
JiahuiChen99 <Jiahui1@hotmail.es>
2024-01-29 15:01:30 +0100

    fix: solve document undefined error in fs back button

--- src/components/common/fs-back-button.tsx
+++ src/components/common/fs-back-button.tsx
@@ -2,13 +2,20 @@
 
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export function BackButton() {
+  const [title, setTitle] = React.useState<string>("");
   const router = useRouter();
 
   const onBackClick = () => {
     router.push(`${window.location.origin}/ext2`);
   };
+
+  useEffect(() => {
+    if (typeof document === "undefined") return;
+    setTitle(document.title);
+  }, []);
 
   return (
     <div
@@ -16,7 +23,7 @@ export function BackButton() {
       className="flex items-center mb-2 cursor-pointer w-fit hover:bg-slate-50 rounded-md p-2 pr-4"
       onClick={onBackClick}>
       <ChevronLeft className="mr-2 h-6 w-6" />
-      <h1 className="font-black text-2xl">{document.title}</h1>
+      <h1 className="font-black text-2xl">{title}</h1>
     </div>
   );
 }
