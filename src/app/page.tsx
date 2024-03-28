20585c2e27e9efef1b8924c5fc4415582e5341ec
JiahuiChen99 <Jiahui1@hotmail.es>
2024-03-28 22:52:34 +0100

    feat: improved hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
+import Link from "next/link";
 import { StorageLottie } from "./home/components/storage-lottie";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <div>
-          <h1 className="text-5xl font-bold">
+      <div className="flex flex-col place-items-center">
+        <div className="space-y-2">
+          <h1 className="text-5xl font-black">
             The interactive docs for File Systems
           </h1>
-          <p className="text-gray-500 ">
+          <p className="text-gray-500">
             An engaging and interactive experience to understand file systems,
             the engine that powers the storage of your data.
           </p>
         </div>
-        <ul>
-          <li>
-            <a href="/ext2">The second Extended File Systems (EXT2)</a>
+        <ul className="my-10 space-y-2.5">
+          <li className="flex">
+            <Link
+              href="/ext2"
+              className="flex cursor-pointer bg-primary text-primary-foreground hover:bg-primary/90 justify-center rounded-md px-10 py-4 w-full">
+              The second Extended File Systems (EXT2)
+            </Link>
           </li>
-          <li>
-            <a href="/fat16">Fat Allocation Table 16 (FAT16)</a>
+          <li className="flex">
+            <Link
+              href="/fat16"
+              className="flex cursor-pointer bg-primary text-primary-foreground hover:bg-primary/90 justify-center rounded-md px-10 py-4 w-full">
+              Fat Allocation Table 16 (FAT16)
+            </Link>
           </li>
         </ul>
       </div>
