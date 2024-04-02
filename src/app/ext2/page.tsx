15776a174978f8dcb649bbad81d0629babdc5034
JiahuiChen99 <Jiahui1@hotmail.es>
2024-04-02 10:49:46 +0200

    feat: add click on a selectaable item toast message in ext2/fat16 pages

--- src/app/ext2/page.tsx
+++ src/app/ext2/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+import { useToast } from "@/components/ui/use-toast";
 import { EXT2 as EXT2Graphic } from "./(ext2-parts)/components/graphic/ext2";
+import { useEffect } from "react";
 
 export default function EXT2() {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    toast({
+      description: "Click on a selectable item to see its details.",
+    });
+  }, []);
+
   return (
     <section className="flex p-10 w-full h-full justify-center align-middle">
       <EXT2Graphic />
--- src/app/fat16/page.tsx
+++ src/app/fat16/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+import { useToast } from "@/components/ui/use-toast";
 import { FAT16 as FAT16Graphic } from "./(fat16-parts)/components/graphic/fat16";
+import { useEffect } from "react";
 
 export default function FAT16() {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    toast({
+      description: "Click on a selectable item to see its details.",
+    });
+  }, []);
+
   return (
     <section className="flex flex-col p-10 w-full h-full justify-center align-middle">
       <FAT16Graphic />
