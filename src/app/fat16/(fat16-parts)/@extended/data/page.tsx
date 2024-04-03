e038bd6ef000b02bd004b9cc5ec3d40b4a88e603
JiahuiChen99 <Jiahui1@hotmail.es>
2024-04-03 14:38:18 +0200

    feat: data regions page

--- src/app/fat16/(fat16-parts)/@extended/data/page.tsx
+++ src/app/fat16/(fat16-parts)/@extended/data/page.tsx
@@ -1,3 +1,9 @@
 export default function Data() {
-  return <></>;
+  return (
+    <section id="root-dir-formulas" className="border p-4 rounded-lg">
+      <p>Note that Cluster index starts from 2!</p>
+      <h3 className="font-bold">Cluster Size</h3>
+      <div>BPB_SecPerClus * BPB_BytsPerSec</div>
+    </section>
+  );
 }
