444929625529cba24abeac8a39a583ca2a0cd050
JiahuiChen99 <Jiahui1@hotmail.es>
2024-04-12 14:31:37 +0200

    feat: improved ext2 graphic

--- src/app/ext2/(ext2-parts)/components/graphic/ext2.tsx
+++ src/app/ext2/(ext2-parts)/components/graphic/ext2.tsx
@@ -7,24 +7,29 @@ export function EXT2() {
   const [isEXT2Expanded, setIsEXT2Expanded] = React.useState<boolean>(false);
   const onBlockGroupClick = () => setIsEXT2Expanded((prevState) => !prevState);
   return (
-    <svg viewBox="0 0 646 149" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 758 166"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
       {isEXT2Expanded && (
         <g className="flex">
           <line
             id="line-2"
-            x1="199.144"
-            y1="44.4787"
-            x2="0.144346"
-            y2="104.479"
+            x1="268.109"
+            y1="44.4879"
+            x2="0.109233"
+            y2="104.488"
             stroke="black"
             strokeDasharray="2 2"
           />
           <line
             id="line-1"
-            x1="316.09"
-            y1="44.5081"
-            x2="645.09"
-            y2="104.508"
+            x1="406.084"
+            y1="44.5071"
+            x2="757.084"
+            y2="104.507"
             stroke="black"
             strokeDasharray="2 2"
           />
@@ -40,139 +45,139 @@ export function EXT2() {
             onClick={onBlockGroupClick}
             className={groupStyle}
             cursor="default">
-            <rect
-              x="528.5"
-              y="0.5"
-              width="117"
-              height="44"
-              fill="white"
-              stroke="black"
-              className={hoverStyle}
-            />
-            <text
-              id="block group n"
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="546.271" y="26.8636">
-                Block Group N
-              </tspan>
-            </text>
+            <g id="block-group-n-rect">
+              <rect
+                x="616.5"
+                y="0.5"
+                width="141"
+                height="44"
+                fill="white"
+                className={hoverStyle}
+              />
+              <rect x="616.5" y="0.5" width="141" height="44" stroke="black" />
+              <text
+                id="block group n"
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="646.271" y="26.8636">
+                  Block Group N
+                </tspan>
+              </text>
+            </g>
           </g>
           <g id="...-g" cursor="default">
-            <rect
-              x="433.5"
-              y="0.5"
-              width="95"
-              height="44"
-              fill="white"
-              stroke="black"
-            />
-            <text
-              id="..."
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="476.043" y="26.8636">
-                ...
-              </tspan>
-            </text>
+            <g id="...-rect">
+              <rect x="546.5" y="0.5" width="69" height="44" fill="white" />
+              <rect x="546.5" y="0.5" width="69" height="44" stroke="black" />
+              <text
+                id="..."
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="576.043" y="26.8636">
+                  ...
+                </tspan>
+              </text>
+            </g>
           </g>
           <g
             id="block-group-2-g"
             cursor="default"
             onClick={onBlockGroupClick}
             className={groupStyle}>
-            <rect
-              x="316.5"
-              y="0.5"
-              width="117"
-              height="44"
-              fill="white"
-              stroke="black"
-              className={hoverStyle}
-            />
-            <text
-              id="block group 2"
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="335.156" y="26.8636">
-                Block Group 2
-              </tspan>
-            </text>
+            <g id="block-group-2-rect">
+              <rect
+                x="406.5"
+                y="0.5"
+                width="139"
+                height="44"
+                fill="white"
+                className={hoverStyle}
+              />
+              <rect x="406.5" y="0.5" width="139" height="44" stroke="black" />
+              <text
+                id="block group 2"
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="436.156" y="26.8636">
+                  Block Group 2
+                </tspan>
+              </text>
+            </g>
           </g>
           <g
             id="block-group-1-g"
             onClick={onBlockGroupClick}
             className={groupStyle}>
-            <rect
-              x="199.5"
-              y="0.5"
-              width="117"
-              height="44"
-              fill="white"
-              stroke="black"
-              className={hoverStyle}
-            />
-            <text
-              id="block group 1"
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="219" y="26.8636">
-                Block Group 1
-              </tspan>
-            </text>
+            <g id="block-group-1-rect">
+              <rect
+                x="268.5"
+                y="0.5"
+                width="137"
+                height="44"
+                fill="white"
+                className={hoverStyle}
+              />
+              <rect x="268.5" y="0.5" width="137" height="44" stroke="black" />
+              <text
+                id="block group 1"
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="298" y="26.8636">
+                  Block Group 1
+                </tspan>
+              </text>
+            </g>
           </g>
           <g
             id="block-group-0-g"
             onClick={onBlockGroupClick}
             className={groupStyle}
             cursor="default">
-            <rect
-              x="82.5"
-              y="0.5"
-              width="117"
-              height="44"
-              fill="white"
-              stroke="black"
-              className={hoverStyle}
-            />
-            <text
-              id="block group 0"
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="101.039" y="26.8636">
-                Block Group 0
-              </tspan>
-            </text>
+            <g id="block-group-0-rect">
+              <rect
+                x="128.5"
+                y="0.5"
+                width="139"
+                height="44"
+                fill="white"
+                className={hoverStyle}
+              />
+              <rect x="128.5" y="0.5" width="139" height="44" stroke="black" />
+              <text
+                id="block group 0"
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="158.039" y="26.8636">
+                  Block Group 0
+                </tspan>
+              </text>
+            </g>
           </g>
           <g id="boot-sector-g" cursor="default">
-            <rect
-              x="0.5"
-              y="0.5"
-              width="82"
-              height="44"
-              fill="white"
-              stroke="black"
-            />
-            <text
-              id="boot sector"
-              fill="black"
-              fontFamily="Inter"
-              fontSize="12"
-              letterSpacing="0em">
-              <tspan x="7.94336" y="26.8636">
-                Boot Sector
-              </tspan>
-            </text>
+            <g id="boot-sector-rect">
+              <rect x="0.5" y="0.5" width="127" height="44" fill="white" />
+              <rect x="0.5" y="0.5" width="127" height="44" stroke="black" />
+              <text
+                id="boot sector"
+                fill="black"
+                fontFamily="Inter"
+                fontSize="12"
+                letterSpacing="0em">
+                <tspan x="30.4434" y="26.8636">
+                  Boot Sector
+                </tspan>
+              </text>
+            </g>
           </g>
         </g>
       </g>
