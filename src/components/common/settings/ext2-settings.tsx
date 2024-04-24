a7a79a1a538f889562f42f241e024ba251eecb3e
JiahuiChen99 <Jiahui1@hotmail.es>
2024-04-24 20:04:47 +0200

    feat: settings tabs components

---
+++ src/components/common/settings/ext2-settings.tsx
@@ -0,0 +1,11 @@
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+export function EXT2Settings() {
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Second Extended File System (EXT2) Settings</DialogTitle>
+      </DialogHeader>
+    </>
+  );
+}
