357cae1a036d098f998f1190669285911fef2cea
JiahuiChen99 <Jiahui1@hotmail.es>
2024-04-27 10:51:15 +0200

    fix: fs back button routes to title

--- src/components/common/fs-back-button.tsx
+++ src/components/common/fs-back-button.tsx
@@ -1,25 +1,35 @@
 "use client";
 
 import { ChevronLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
-import { Skeleton } from "../ui/skeleton";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
 
-export function BackButton({ backToPath = "/" }: { backToPath: string }) {
-  const [title, setTitle] = React.useState<string>("");
+const routesToTitle: { [index: string]: string } = {
+  "super-block": "Super block",
+  "block-group-descriptor": "Block Group Descriptor",
+  "block-bitmap": "Block Bitmap",
+  "inode-bitmap": "Inode Bitmap",
+  "inode-table": "Inode Table",
+  "data-blocks": "Data Blocks",
+  "reserved-area": "Reserved Area",
+  fat: "FAT",
+  "root-directory": "Root directory",
+  data: "Data (Files and directories)",
+};
+
+export function BackButton({
+  backToPath = "/",
+}: Readonly<{ backToPath: string }>) {
+  const pathname = usePathname();
   const router = useRouter();
 
   const onBackClick = () => {
     router.push(`${window.location.origin}${backToPath}`);
   };
 
-  useEffect(() => {
-    if (typeof document === "undefined") return;
-    setTitle(document.title);
-  }, []);
-
-  if (title === "")
-    return <Skeleton className="mb-2 w-full h-[48px] rounded-md" />;
+  const title: keyof typeof routesToTitle = React.useMemo(() => {
+    return pathname.split("/")[2];
+  }, [pathname]);
 
   return (
     <div
@@ -27,7 +37,7 @@ export function BackButton({ backToPath = "/" }: { backToPath: string }) {
       className="flex items-center mb-2 cursor-pointer w-fit hover:bg-slate-50 rounded-md p-2 pr-4"
       onClick={onBackClick}>
       <ChevronLeft className="mr-2 h-6 w-6" />
-      <h1 className="font-black text-2xl">{title}</h1>
+      <h1 className="font-black text-2xl">{routesToTitle[title] ?? "Hello"}</h1>
     </div>
   );
 }
