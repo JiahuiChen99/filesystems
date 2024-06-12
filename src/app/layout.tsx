614e89d31ff2e99d61dfe692c39d78ec9fcf1aab
JiahuiChen99 <Jiahui1@hotmail.es>
2024-06-12 12:15:56 +0200

    feat: keywords meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "File systems",
   description: "Advanced Operating Systems file systems project documentation",
+  keywords: [
+    "File Systems",
+    "Ext2",
+    "Fat16",
+    "FS",
+    "Linux",
+    "Windows",
+    "File & Disk Management",
+    "Operating systems",
+    "Computer Science",
+    "Data Structures",
+    "DSA",
+    "La Salle URL",
+  ],
 };
 
 export default function RootLayout({
@@ -23,13 +37,13 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <TooltipProvider>
-      <html lang="en">
-        <body className={clsx("flex flex-col w-full h-dvh", inter.className)}>
-          <Navbar />
-          {children}
-          <Toaster />
-        </body>
-      </html>
+        <html lang="en">
+          <body className={clsx("flex flex-col w-full h-dvh", inter.className)}>
+            <Navbar />
+            {children}
+            <Toaster />
+          </body>
+        </html>
       </TooltipProvider>
     </StoreProvider>
   );
