8293a2c16c0877777c149025aded2f7d9806de84
JiahuiChen99 <Jiahui1@hotmail.es>
2024-06-12 11:30:50 +0200

    feat: ta card multi-email - copy icon - anchor mailto - styles

--- src/app/(misc)/information/components/teacher-assistant-card.tsx
+++ src/app/(misc)/information/components/teacher-assistant-card.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { TA } from "../data/ta-data";
+import { Copy } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export function TeacherAssistantCard({ ta }: { ta: TA }) {
+  const copyPostURLToClipboard = async (email: string) => {
+    try {
+      await navigator.clipboard.writeText(email);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex items-center space-x-4">
       <Avatar className="w-16 h-16">
@@ -10,7 +28,32 @@ export function TeacherAssistantCard({ ta }: { ta: TA }) {
       </Avatar>
       <div>
         <div className="font-bold">{ta.name}</div>
-        <div>{ta.email}</div>
+        {ta.email.map((mail) => (
+          <div className="flex" key={mail}>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="w-5 h-5 group"
+                  onClick={() => copyPostURLToClipboard(mail)}
+                >
+                  <Copy
+                    size={14}
+                    color="gray"
+                    className="group-hover:stroke-black"
+                  />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Copy address</p>
+              </TooltipContent>
+            </Tooltip>
+            <a className="ml-2 text-gray-500" href={`mailto:${mail}`}>
+              {mail}
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
