fc831b68e221bcd884bfbd5961a8b4fd972acbc0
Jiahui Chen <jiahui@jiahuichen.dev>
2024-11-16 18:34:48 +0100

    feat: :sparkles: switch displayed fs canvas with set interval in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 import { CanvasBitmapNode } from "@/components/canvas/canvas-bitmap-node";
 import { CanvasTableNode } from "@/components/canvas/canvas-table-node";
 import { FSCanvas } from "@/components/canvas/fs-canvas";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { FS_NAMES } from "@/domain/consts";
 import {
   Background,
   BackgroundVariant,
@@ -11,6 +13,7 @@ import {
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { useRouter } from "next/navigation";
+import React from "react";
 import { fsNodesConfig } from "./canvas/[fs_id]/canvas-node-config/fs-canvas-nodes-config";
 import { FSListItem } from "./home/components/fs-list-item";
 import { FSList } from "./home/data/fs-list-data";
@@ -20,12 +23,8 @@ const nodeTypes = {
   "canvas-bitmap": CanvasBitmapNode,
 };
 
-const DEFAULT_FS = "ext-2";
-const { nodes: _nodes, edges: _edges } = fsNodesConfig[DEFAULT_FS];
-
+const DEFAULT_FS: FSKeys = "ext-2";
 const homePageFSCanvasConfig: ReactFlowProps = {
-  nodes: _nodes,
-  edges: _edges,
   nodeTypes,
   proOptions: { hideAttribution: true },
   edgesReconnectable: false,
@@ -38,10 +37,32 @@ const homePageFSCanvasConfig: ReactFlowProps = {
   panOnScroll: true,
   selectionOnDrag: true,
   minZoom: 0,
+  maxZoom: 0.5,
 };
+
+type FSKeys = keyof typeof fsNodesConfig;
+const allFSKeys: Array<FSKeys> = Object.keys(fsNodesConfig) as Array<FSKeys>;
+const SWITCH_FS_TIME = 10_000;
+let initFSId = 0;
 
 export default function Home() {
   const router = useRouter();
+  const [currentFSKey, setCurrentFSKey] = React.useState<FSKeys>(DEFAULT_FS);
+  const { nodes: _nodes, edges: _edges } = fsNodesConfig[currentFSKey];
+
+  const switchFS = () => {
+    initFSId++;
+    if (initFSId === allFSKeys.length) {
+      initFSId = 0;
+    }
+    setCurrentFSKey(allFSKeys[initFSId]);
+  };
+
+  React.useEffect(() => {
+    const timerId = setInterval(switchFS, SWITCH_FS_TIME);
+    return () => clearInterval(timerId);
+  }, []);
+
   return (
     <main className="flex flex-col md:flex-row w-full h-full px-10 py-20 justify-between space-x-2">
       <div className="flex flex-col place-items-center">
@@ -61,8 +82,13 @@ export default function Home() {
         </ul>
       </div>
       <div className="flex flex-col-reverse w-full md:flex-col">
+        <Badge className="w-fit mb-2" variant="default">
+          {FS_NAMES[currentFSKey]}
+        </Badge>
         <div className="border rounded-md max-w-full h-96 max-h-full md:h-full">
-          <FSCanvas props={homePageFSCanvasConfig}>
+          <FSCanvas
+            props={{ ...homePageFSCanvasConfig, nodes: _nodes, edges: _edges }}
+          >
             <Background
               variant={"dots" as BackgroundVariant}
               gap={12}
@@ -73,11 +99,14 @@ export default function Home() {
         <div className="flex space-x-2 place-content-end mb-2 md:mt-2">
           <Button
             variant="outline"
-            onClick={() => router.push("/canvas/ext-2")}
+            onClick={() => router.push(`/canvas/${currentFSKey}`)}
           >
             View it in Canvas
           </Button>
-          <Button variant="outline" onClick={() => router.push("/ext2")}>
+          <Button
+            variant="outline"
+            onClick={() => router.push(`/${currentFSKey}`)}
+          >
             Go to docs
           </Button>
         </div>
---
+++ src/domain/consts.ts
@@ -0,0 +1,4 @@
+export const FS_NAMES = {
+  "ext-2": "Extended File System 2 (EXT 2)",
+  "fat-16": "File Allocation 16-bit",
+};
