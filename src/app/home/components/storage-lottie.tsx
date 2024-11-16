5d741f6f9bbfeaff8f7aff0a89a39761d0be6a78
Jiahui Chen <jiahui@jiahuichen.dev>
2024-11-16 18:32:33 +0100

    refactor: :fire: remove lottie dependency and lottie file animation

--- src/app/home/components/storage-lottie.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { useLootie } from "../hooks/useLootie";
-
-export function StorageLottie() {
-  const { attachAnime } = useLootie();
-
-  return <div className="w-auto h-auto" ref={attachAnime} />;
-}
